9126698e83455a20ca85687042c65bfe83a178b9
GuillermoR26 <renteriabravojgg102@gmail.com>
2024-08-07 14:24:39 -0700

    Update configuration 2nd.py

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE ="https://cnt-59270864-73dd-4f1f-9f2c-37ec4c6fc81b.containerhub.tripleten-services.com" # Inserta tu dirección de URL sin la barra diagonal al final
+URL_SERVICE ="https://cnt-ccaa0a93-24cf-40e3-874e-34c58ddd4823.containerhub.tripleten-services.com" # Inserta tu dirección de URL sin la barra diagonal al final
 CREATE_USER_PATH = "/api/v1/users" # Almacena la ruta para crear un usuario o usuaria en esta variable
 KITS_PATH = "/api/v1/kits" # Almacena la ruta para crear un kit en esta variable
 API_DOCS = "https://cnt-2caf3ae4-48e4-4045-86be-36fd084a6fe2.containerhub.tripleten-services.com/docs/#api-Main.User-CreateUser"
